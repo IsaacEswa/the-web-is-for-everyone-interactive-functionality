b1cf5821cf6b63ff8f543cb10a712c054df9f062
IsaacEswa <isaaceswa@gmail.com>
2026-03-18 16:28:45 +0100

    search function gefixt #20

--- server.js
+++ server.js
@@ -50,12 +50,30 @@ app.get('/', async function (request, response) {
   response.render('index.liquid', { stories: apiStoriesResponseJSON.data, district: district, slug: slug })
 })
 
+app.get('/search', async function (request, response) {
+  const search = request.query.search || ''
+
+  const params = {
+    'fields': 'cover, date, title, intro, status, district, slug',
+    ...(search && { 'filter[title][_icontains]': search }),
+    'filter[status][_eq]': 'published',
+    limit: -1,
+  }
+
+  const apiStoriesResponse = await fetch('https://fdnd-agency.directus.app/items/buurtcampuskrant_stories?' + new URLSearchParams(params))
+  const apiStoriesResponseJSON = await apiStoriesResponse.json()
+  // console.log(personResponseJSON.data)
+  console.log(apiStoriesResponseJSON.data.length)
+
+  response.render('search.liquid', { stories: apiStoriesResponseJSON.data, search })
+
+})
+
+
 app.get('/:district', async function (request, response) {
 
   const district = request.params.district
   const slug = request.params.slug
-
-
 
   const params = {
     // Sorteren op datum, van nieuw naar oud (dus met een minteken ervoor)
@@ -101,26 +119,6 @@ app.get('/:district/:slug', async function (request, response) {
 
   response.render('details.liquid', { stories: apiStoriesResponseJSON.data, district: district, slug: slug })
 })
-
-app.get('/search', async function (request, response) {
-  const search = request.query.search || ''
-
-  const params = {
-    'fields': 'cover, date, title, intro, status, district, slug',
-    ...(search && { 'filter[title][_icontains]': search }),
-    'filter[status][_eq]': 'published',
-    limit: -1,
-  }
-
-  const apiStoriesResponse = await fetch('https://fdnd-agency.directus.app/items/buurtcampuskrant_stories?' + new URLSearchParams(params))
-  const apiStoriesResponseJSON = await apiStoriesResponse.json()
-  // console.log(personResponseJSON.data)
-  console.log(apiStoriesResponseJSON.data.length)
-
-  response.render('search.liquid', { stories: apiStoriesResponseJSON.data, search })
-
-})
-
 // Maak een POST route voor de index; hiermee kun je bijvoorbeeld formulieren afvangen
 // Hier doen we nu nog niets mee, maar je kunt er mee spelen als je wilt
 app.post('/', async function (request, response) {
