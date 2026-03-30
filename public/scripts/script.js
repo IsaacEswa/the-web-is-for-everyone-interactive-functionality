b2552a5c0526178ac3ec843d1d03c335c1791ffb
IsaacEswa <isaaceswa@gmail.com>
2026-03-30 15:29:14 +0200

    filter submit na selecteren #

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -7,4 +7,5 @@
 // });
 
 document.getElementById('filter-button').hidden = true;
-document.getElementById('targetgroup-select').addEventListener('change', function () { this.form.submit() })
+document.getElementById('targetgroup-select').addEventListener('change', function () { this.form.submit() });
+document.getElementById('sort-select').addEventListener('change', function () { this.form.submit() })
